a329012cbbc4cc881ad61794ef0df5ef01724101
DavitEgoian <dato.egoyan@gmail.com>
2025-04-22 15:22:23 +0400

    Watermarker class has been added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import filedialog, simpledialog, messagebox
 from PIL import Image, ImageDraw, ImageFont
 
+import Watermarker
+
 root = tk.Tk()
 root.title("Watermarker")
 root.geometry("300x200")
