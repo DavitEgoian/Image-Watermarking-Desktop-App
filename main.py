5d4c001f88390341a5cc08acf052ebf165549ad9
DavitEgoian <dato.egoyan@gmail.com>
2025-04-25 22:35:32 +0400

    Main py logic has been added

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from watermarker_app import WatermarkerApp
 
-root = tk.Tk()
-root.title("Watermarker")
-root.geometry("300x200")
+def main():
+    root = tk.Tk()
+    root.title("Watermarker")
+    root.geometry("300x200")
+
+    app = WatermarkerApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
